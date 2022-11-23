70b5e399d5c7f1c47e41fc052e743a27adbf10b5
Bouilly Bill <bill.bouilly@hotmail.fr>
2022-11-23 21:36:31 +0100

    TP
    TP programmation avancée

---
+++ TP01/SchemaParticulier.java
@@ -0,0 +1,14 @@
+
+public class SchemaParticulier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point p1 = new Point(3, 2);
+		Point p2 = new Point(6, 9);
+		Segment s1 = new Segment(p1, p2);
+		Point p3 = new Point(11, 4);
+		Segment s2 = new Segment(p2, p3);
+		Segment s3 = new Segment(p3, p1);
+	}
+
+}
